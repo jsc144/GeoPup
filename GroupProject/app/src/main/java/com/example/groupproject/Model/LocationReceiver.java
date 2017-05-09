39466fcaaa38b892ba1c1735f4f4f371615c96b0
jsc144 <jsc144@gmail.com>
2017-05-08 23:11:48 -0400

    Backend Extra Stats
    +added health.hunger, and joy
    +added possibility of pet data being reset

--- GroupProject/app/src/main/java/com/example/groupproject/Model/LocationReceiver.java
+++ GroupProject/app/src/main/java/com/example/groupproject/Model/LocationReceiver.java
@@ -21,6 +21,7 @@ package com.example.groupproject.Model;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,6 +30,15 @@ import com.commonsware.cwac.locpoll.LocationPollerResult;
 import com.example.groupproject.service.impl.ZoneService;
 
 import java.util.List;
+import java.util.Random;
+
+import static com.example.groupproject.Model.Constants.HEALTH_INCREMENT;
+import static com.example.groupproject.Model.Constants.HUNGER_INCREMENT;
+import static com.example.groupproject.Model.Constants.JOY_INCREMENT;
+import static com.example.groupproject.Model.Constants.MAX_HEALTH;
+import static com.example.groupproject.Model.Constants.MAX_HUNGER;
+import static com.example.groupproject.Model.Constants.MAX_JOY;
+import static com.example.groupproject.Model.Constants.POINT_INCREMENT;
 
 public class LocationReceiver extends BroadcastReceiver {
     private Context _context;
@@ -64,19 +74,47 @@ public class LocationReceiver extends BroadcastReceiver {
         Log.d("MSG", msg +" "+ points);
     }
     private void countPoints(List<Zone> zones, double longitude, double latitude){
+        android.content.SharedPreferences pm = _context.getSharedPreferences("GeoCat",0);
+        long hunger = pm.getLong("hunger",MAX_HUNGER);
+        if(hunger <= 0){
+            long health = pm.getLong("health",MAX_HEALTH);
+            if(health == 0){
+                resetStats();
+            }else{
+                addHealth(HEALTH_INCREMENT);
+            }
+        }else{
+            addHunger(HUNGER_INCREMENT);
+        }
         for (Zone zone:zones) {
             if(inZone(zone,longitude,latitude))
             {
-                if(zone.getZoneType() != 0)
-                    addPoints(-1);
-                else
-                    addPoints(1);
+                if(zone.getZoneType() != 0) {
+                    addJoy(JOY_INCREMENT);
+                    long joy = pm.getLong("joy",MAX_JOY);
+                    if(joy <= 0){
+                        //10% chance to reset statistics
+                        Random r = new Random();
+                        if(r.nextInt(100-0) >= 90){
+                            resetStats();
+                        }
+                    }
+                }
+                else {
+                    addPoints(POINT_INCREMENT);
+                }
             }
             Log.d("Points", Long.toString(points));
         }
-
-
-
+    }
+    private void resetStats(){
+        android.content.SharedPreferences pm = _context.getSharedPreferences("GeoCat",0);
+        android.content.SharedPreferences.Editor editor = pm.edit();
+        editor.putLong("health", MAX_HEALTH);
+        editor.putLong("hunger", MAX_HUNGER);
+        editor.putLong("joy", MAX_JOY);
+        editor.commit();
+        //TODO:reset name
     }
     private void addPoints(long pointValue){
         //TODO:Make persistent
@@ -85,6 +123,30 @@ public class LocationReceiver extends BroadcastReceiver {
         points = points + pointValue;
         android.content.SharedPreferences.Editor editor = pm.edit();
         editor.putLong("points",points);
+        editor.commit();
+    }
+    private void addHunger(long hungerValue){
+        SharedPreferences pm = _context.getSharedPreferences("GeoCat",0);
+        long hunger = pm.getLong("hunger",MAX_HUNGER);
+        hunger = hunger + hungerValue;
+        SharedPreferences.Editor editor = pm.edit();
+        editor.putLong("hunger",hunger);
+        editor.commit();
+    }
+    private void addJoy(long joyValue){
+        SharedPreferences pm = _context.getSharedPreferences("GeoCat",0);
+        long joy = pm.getLong("joy",MAX_JOY);
+        joy += joyValue;
+        SharedPreferences.Editor editor = pm.edit();
+        editor.putLong("joy",joy);
+        editor.commit();
+    }
+    private void addHealth(long healthValue){
+        SharedPreferences pm = _context.getSharedPreferences("GeoCat",0);
+        long health= pm.getLong("health",MAX_HEALTH);
+        health += healthValue;
+        SharedPreferences.Editor editor = pm.edit();
+        editor.putLong("health",health);
         editor.commit();
     }
     private List<Zone> getZones(){
